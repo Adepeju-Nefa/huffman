8f195769954b3d8e649883d5e14dfb9a4252241a
Adepeju-Nefa <79343740+Adepeju-Nefa@users.noreply.github.com>
2021-05-02 18:08:23 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,25 +8,9 @@
  -------------------------------------
  Authors: Group 10
 
- Name: Muhammad Rufaid Jugoo
- Student ID: 180174860
- Email: jugo4860@mylaurier.ca
-
- Name: Lovette Oyewole
- Student ID: 190888960
- Email: oyew8960@mylaurier.ca
-
- Name: John Oyelade
- Student ID: 180284190
- Email: oyel4190@mylaurier.ca
-
  Name: Adepeju Olowonefa
  Student ID: 193049300
  Email: olow9300@mylaurier.ca
-
- Name: Esseoghene Ogbaran
- Student ID: 190267850
- Email: ogba7850@mylaurier.ca
 
  Version  2020-11-28
  -------------------------------------
